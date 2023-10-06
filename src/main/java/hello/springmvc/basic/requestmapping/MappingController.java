d2b01f15f19b2e15d5d78fc7457516be8f228e4d
Seunghyun Kim <tmdgus9701@naver.com>
2023-10-06 17:51:23 +0900

    사용하지 않는 import 제거

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,10 +1,8 @@
 package hello.springmvc.basic.requestmapping;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.MediaType.*;
 
 @Slf4j
 @RestController
