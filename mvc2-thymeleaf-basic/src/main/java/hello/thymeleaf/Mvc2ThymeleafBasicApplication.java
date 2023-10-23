e853413f36b14f3b4629903651afbb952874f304
Seunghyun Kim <tmdgus9701@naver.com>
2023-10-23 13:23:59 +0900

    mvc2 thymeleaf start project

---
+++ mvc2-thymeleaf-basic/src/main/java/hello/thymeleaf/Mvc2ThymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc2ThymeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc2ThymeleafBasicApplication.class, args);
+	}
+
+}
